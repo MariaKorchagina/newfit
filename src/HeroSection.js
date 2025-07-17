7226c5214950531d197d8c96245ab207058d9302
Maria <maria@Vladislavs-MacBook-Pro.local>
2025-07-18 00:28:18 +0300

    Initial project commit

---
+++ src/HeroSection.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import './HeroSection.css';
+
+const ArrowIcon = () => (
+  <img src="https://gymteam.kinescopecdn.net/img/l/2024/vitrina/arrow.svg" alt="arrow" style={{ width: 44, height: 44, marginLeft: 18 }} />
+);
+
+const Arrow2Button = () => {
+  const handleClick = () => {
+    const el = document.querySelector('.big-center-text');
+    if (el) {
+      el.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+  return (
+    <div className="hero-arrow2-btn-wrap">
+      <button className="hero-arrow2-btn" tabIndex={-1} aria-label="Прокрутить вниз" onClick={handleClick}>
+        <img src="https://gymteam.kinescopecdn.net/img/l/2024/vitrina/arrow2.svg" alt="arrow2" />
+      </button>
+    </div>
+  );
+};
+
+const HeroSection = () => {
+  const handleScrollToPrograms = () => {
+    const el = document.querySelector('.big-center-text');
+    if (el) {
+      el.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
+  return (
+    <div className="hero-row">
+      <div className="hero-block hero-block--left">
+        <h1 className="hero-title">ОНЛАЙН-ТРЕНИРОВКИ ДЛЯ ДОМА И ЗАЛА С ЛИЛИЕЙ ЯЦКОЙ</h1>
+        <p className="hero-subtitle">Авторские фитнес-программы под разные<br/>цели для любого уровня подготовки</p>
+        <button className="hero-btn" onClick={handleScrollToPrograms}>
+          <span className="hero-btn-text" style={{whiteSpace: 'nowrap'}}>Выбрать программу</span>
+          <ArrowIcon />
+        </button>
+        <div className="hero-stats">Начни свой путь к здоровью<br/>и уверенности вместе со мной!</div>
+      </div>
+      <div className="hero-block hero-block--right">
+        <img src={process.env.PUBLIC_URL + '/pic/trainer6.jpg'} alt="Тренер" className="hero-trainer-photo" />
+      </div>
+      <Arrow2Button />
+    </div>
+  );
+};
+
+export default HeroSection; 
